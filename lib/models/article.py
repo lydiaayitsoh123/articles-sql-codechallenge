9e506baa0a172fbdd934c4a67f5778f8eb0b6d4d
LYDIA <lydiaayitsoh@gmail.com>
2025-05-26 22:12:28 +0300

    fixed article

--- lib/models/article.py
+++ lib/models/article.py
@@ -1,6 +1,52 @@
+# lib/models/article.py
+from lib.db.connection import get_connection
+
 class Article:
     def __init__(self, id, title, author_id, magazine_id):
         self.id = id
         self.title = title
         self.author_id = author_id
         self.magazine_id = magazine_id
+
+    @classmethod
+    def all(cls):
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM articles")
+        rows = cursor.fetchall()
+        return [cls(row["id"], row["title"], row["author_id"], row["magazine_id"]) for row in rows]
+
+    @classmethod
+    def find_by_author(cls, author_id):
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM articles WHERE author_id = ?", (author_id,))
+        rows = cursor.fetchall()
+        return [cls(row["id"], row["title"], row["author_id"], row["magazine_id"]) for row in rows]
+
+    @classmethod
+    def find_by_magazine(cls, magazine_id):
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM articles WHERE magazine_id = ?", (magazine_id,))
+        rows = cursor.fetchall()
+        return [cls(row["id"], row["title"], row["author_id"], row["magazine_id"]) for row in rows]
+
+    @classmethod
+    def create(cls, title, author_id, magazine_id):
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute(
+            "INSERT INTO articles (title, author_id, magazine_id) VALUES (?, ?, ?)",
+            (title, author_id, magazine_id)
+        )
+        conn.commit()
+        return cls(cursor.lastrowid, title, author_id, magazine_id)
+
+    def author(self):
+        from lib.models.author import Author
+        return Author.find_by_id(self.author_id)
+
+    def magazine(self):
+        from lib.models.magazine import Magazine
+        return Magazine.find_by_id(self.magazine_id)
